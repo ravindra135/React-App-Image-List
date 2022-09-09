ebf88aa0ddf4141993d037230a1b5d7f3d4378ac
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-09 17:42:04 +0530

    user input forms and event handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import SearchInput from './Searchinput';
 
-// Functional Component;
-const App = () => {
-    return (
-        <div className='ui container' style={{ marginTop:'30px' }}>
-            <SearchInput />
-        </div>
-    )
+// Functional Component converted to class-based Component;
+class App extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+        
+        this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    }
+
+    // Callback Function on App
+    onSearchSubmit(entry) {
+        console.log(entry)
+    }
+
+    render() {
+        return (
+            <div className='ui container' style={{ marginTop:'30px' }}>
+                <SearchInput onSearchSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
+    
 }
 
 // Making available to the component;
